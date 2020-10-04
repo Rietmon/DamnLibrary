cbca2ca2d9d03204ea7cb35a340d1ecfe5c44554
Rietmon <www.sakhno23@gmail.com>
2020-10-04 15:35:41 +0300

    Prefabs field

---
+++ Game/Prefab/Editor/PrefabPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Prefab<>))]
+public class PrefabPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var prefabRect = new Rect(position.x, position.y, position.width, position.height);
+
+        EditorGUI.PropertyField(prefabRect, property.FindPropertyRelative("prefabObject"), GUIContent.none);;
+
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
--- Game/Prefab/Prefab.cs
+++ Game/Prefab/Prefab.cs
@@ -1,7 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-public class Prefab
+[Serializable]
+public class Prefab<T> where T : Component
 {
+    public T PrefabObject => prefabObject;
+
+    public Transform DefaultParent
+    {
+        get => defaultParent;
+        set => defaultParent = value;
+    }
+    
+    [SerializeField] private T prefabObject;
+
+    [SerializeField] private Transform defaultParent;
+
+    public T Instantiate() => 
+        Instantiate(Vector3.zero, Quaternion.identity, Vector3.one, DefaultParent);
+
+    public T Instantiate(Vector3 position) => 
+        Instantiate(position, Quaternion.identity, Vector3.one, DefaultParent);
+
+    public T Instantiate(Quaternion rotation) => 
+        Instantiate(Vector3.zero, rotation, Vector3.one, DefaultParent);
+
+    public T Instantiate(Transform parent) =>
+        Instantiate(Vector3.zero, Quaternion.identity, Vector3.one, parent);
+
+    public T Instantiate(Vector3 position, Quaternion rotation) =>
+        Instantiate(position, rotation, Vector3.one, DefaultParent);
+
+    public T Instantiate(Vector3 position, Vector3 scale) =>
+        Instantiate(position, Quaternion.identity, scale, DefaultParent);
+
+    public T Instantiate(Quaternion rotation, Vector3 scale) =>
+        Instantiate(Vector3.zero, rotation, scale, DefaultParent);
+
+    public T Instantiate(Vector3 position, Quaternion rotation, Vector3 scale) =>
+        Instantiate(position, rotation, scale, DefaultParent);
+
+    public T Instantiate(Vector3 position, Quaternion rotation, Vector3 scale, Transform parent)
+    {
+        var result = Object.Instantiate(prefabObject);
+        var transform = result.transform;
+        
+        transform.position = position;
+        transform.rotation = rotation;
+        transform.localScale = scale;
+        transform.parent = parent;
+
+        return result;
+    }
 }
