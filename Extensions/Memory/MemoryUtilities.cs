7c7d76ce1010ea912e6832c5797e2ec0150cc626
Ilya Sakhno <www.sakhno23@gmail.com>
2021-09-30 00:34:18 +0300

    Update

--- Extensions/Memory/MemoryUtilities.cs
+++ Extensions/Memory/MemoryUtilities.cs
@@ -4,11 +4,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using UnityEngine;
 
 namespace Rietmon.Extensions
 {
     public static unsafe class MemoryUtilities
     {
+#if DEBUG_ENABLE_WATCHING_MEMORY_ALLOCATING
+        public static readonly List<AllocatedPointerDebugInfo> allocatedPointers = new List<AllocatedPointerDebugInfo>();
+#endif
+        
 #if ENABLE_WINDOWS_UTILITIES
         [DllImport("kernel32.dll", SetLastError = true, EntryPoint = "CopyMemory")]
         public static extern void CopyMemory(void* destination, void* source, int size);
@@ -20,9 +25,31 @@ namespace Rietmon.Extensions
         public static extern bool IsBadReadPointer(void* pointer, int length);
 #endif
 
-        public static void* Allocate(int count) => Marshal.AllocHGlobal(count).ToPointer();
+        public static void* Allocate(int count) => InternalAllocate(count);
 
-        public static void Free(void* pointer) => Marshal.FreeHGlobal(new IntPtr(pointer));
+        private static void* InternalAllocate(int count)
+        {
+#if !DEBUG_ENABLE_WATCHING_MEMORY_ALLOCATING
+            return Marshal.AllocHGlobal(count).ToPointer();
+#else
+            var intPointer = Marshal.AllocHGlobal(count);
+            allocatedPointers.Add(intPointer);
+            return intPointer.ToPointer();
+#endif
+        }
+
+        public static void Free(void* pointer) => InternalFree(pointer);
+
+        private static void InternalFree(void* pointer)
+        {
+#if !DEBUG_ENABLE_WATCHING_MEMORY_ALLOCATING
+            Marshal.FreeHGlobal(new IntPtr(pointer));
+#else
+            var intPointer = new IntPtr(pointer);
+            allocatedPointers.Remove(intPointer);
+            Marshal.FreeHGlobal(new IntPtr(pointer));
+#endif
+        }
 
 #if !ENABLE_WINDOWS_UTILITIES
         public static void CopyMemory(void* destination, void* source, int size)
@@ -47,16 +74,16 @@ namespace Rietmon.Extensions
 
             if (type.IsValueType)
             {
-                var pointer = Marshal.AllocHGlobal(Marshal.SizeOf<T>());
-                Marshal.StructureToPtr(value, pointer, true);
-                return pointer.ToPointer();
+                var pointer = Allocate(Marshal.SizeOf<T>());
+                Marshal.StructureToPtr(value, new IntPtr(pointer), true);
+                return pointer;
             }
 
             var handler = GCHandle.Alloc(value);
             return GCHandle.ToIntPtr(handler).ToPointer();
         }
 
-        public static T FromPointer<T>(void* pointer, int length = -1, Encoding encoding = null)
+        public static T FromPointer<T>(void* pointer, bool freeAfterConverting = false, int length = -1, Encoding encoding = null)
         {
             var type = typeof(T);
             if (type == typeof(string))
@@ -64,20 +91,26 @@ namespace Rietmon.Extensions
                 var data = (byte*)pointer;
                 encoding ??= Encoding.UTF8;
                 var result = (T)(object)encoding.GetString(data, length);
+                if (freeAfterConverting)
+                    Free(pointer);
                 return result;
             }
 
             if (type.IsValueType)
             {
                 var result = Marshal.PtrToStructure<T>(new IntPtr(pointer));
+                if (freeAfterConverting)
+                    Free(pointer);
                 return result;
             }
 
             var handler = GCHandle.FromIntPtr(new IntPtr(pointer));
+            if (freeAfterConverting)
+                Free(pointer);
             return (T)handler.Target;
         }
 
-        public static T[] FromPointerToArray<T>(void* pointer, int elementsCount) where T : unmanaged
+        public static T[] FromPointerToArray<T>(void* pointer, int elementsCount, bool freeAfterConverting = false) where T : unmanaged
         {
             if (elementsCount <= 0)
                 return Array.Empty<T>();
@@ -87,10 +120,13 @@ namespace Rietmon.Extensions
             for (var i = 0; i < elementsCount; i++)
                 result[i] = managedPointer[i];
 
+            if (freeAfterConverting)
+                Free(pointer);
+
             return result;
         }
 
-        public static T[] FromPointerToStructureArray<T>(void* pointer, int elementsCount) where T : struct
+        public static T[] FromPointerToStructureArray<T>(void* pointer, int elementsCount, bool freeAfterConverting = false) where T : struct
         {
             if (elementsCount <= 0)
                 return Array.Empty<T>();
@@ -101,12 +137,15 @@ namespace Rietmon.Extensions
             for (var i = 0; i < elementsCount; i++)
                 result[i] = Marshal.PtrToStructure<T>(IntPtr.Add(new IntPtr(pointer), i * structureSize));
 
+            if (freeAfterConverting)
+                Free(pointer);
+
             return result;
         }
 
-        public static string FromBytesToString(void* pointer, int length)
+        public static string FromBytesToString(void* pointer, int length, bool freeAfterConverting = false)
         {
-            var bytes = FromPointerToStructureArray<byte>(pointer, length);
+            var bytes = FromPointerToStructureArray<byte>(pointer, length, freeAfterConverting);
             return Encoding.UTF8.GetString(bytes, 0, length);
         }
 
@@ -124,5 +163,36 @@ namespace Rietmon.Extensions
 
         public static int GetCharPointerLength(char* pointer) => new string(pointer).Length;
     }
+
+#if DEBUG_ENABLE_WATCHING_MEMORY_ALLOCATING
+    public readonly unsafe struct AllocatedPointerDebugInfo
+    {
+        public readonly IntPtr allocatedPointer;
+
+        public readonly string stackTrace;
+    
+        public AllocatedPointerDebugInfo(IntPtr pointer)
+        {
+            allocatedPointer = pointer;
+            stackTrace = Environment.StackTrace;
+        }
+
+        public bool Equals(AllocatedPointerDebugInfo other) => this == other;
+
+        public override bool Equals(object obj) => obj is AllocatedPointerDebugInfo other && this == other;
+
+        public override int GetHashCode() => (allocatedPointer.GetHashCode() * 397) ^ (stackTrace != null ? stackTrace.GetHashCode() : 0);
+
+        public static bool operator ==(AllocatedPointerDebugInfo left, AllocatedPointerDebugInfo right) =>
+            left.allocatedPointer == right.allocatedPointer;
+
+        public static bool operator !=(AllocatedPointerDebugInfo left, AllocatedPointerDebugInfo right) => !(left == right);
+
+        public static implicit operator AllocatedPointerDebugInfo(IntPtr pointer) =>
+            new AllocatedPointerDebugInfo(pointer);
+        public static implicit operator AllocatedPointerDebugInfo(void* pointer) =>
+            new AllocatedPointerDebugInfo(new IntPtr(pointer));
+    }
+#endif
 }
 #endif
