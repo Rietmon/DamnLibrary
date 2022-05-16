7d898b85576106871d3c6ec1c192272cb82804de
Ilya <www.sakhno23@gmail.com>
2022-05-17 01:10:43 +0200

    Update

---
+++ Extensions/Memory/Arrays/ManagedArray.cs
@@ -0,0 +1,120 @@
+﻿#if ENABLE_MEMORY_UTILITIES
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Rietmon.Extensions
+{
+    public unsafe class ManagedBytesArray<T> : IUnsafeArray<T, byte> where T : struct
+    {
+        public int Length
+        {
+            get => length;
+            set
+            {
+                ((IUnsafeArray<T, byte>)this).Resize(value, length);
+                length = value;
+            }
+        }
+
+        ushort IUnsafeArray<T, byte>.StructSize => structSize;
+
+        byte* IUnsafeArray<T, byte>.ArrayPointer
+        {
+            get => arrayPointer;
+            set => arrayPointer = value;
+        }
+
+        private readonly ushort structSize;
+        
+        private int length;
+        private byte* arrayPointer;
+
+        public ManagedBytesArray(int length)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>();
+            this.length = length;
+            arrayPointer = (byte*)MemoryUtilities.Allocate(structSize * length);
+        }
+        
+        public ManagedBytesArray(T[] array)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>(); 
+            length = array.Length;
+            arrayPointer = (byte*)Marshal.UnsafeAddrOfPinnedArrayElement(array, 0).ToPointer();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= length)
+                    return default;
+            
+                return MemoryUtilities.FromPointer<T>(arrayPointer + index * structSize);
+            }
+            set
+            {
+                if (index >= length)
+                    return;
+            
+                MemoryUtilities.CopyMemory(arrayPointer + index * structSize, value.ToPointer(), structSize);
+            }
+        }
+
+        public T[] ToArray() => MemoryUtilities.FromPointerToStructureArray<T>(arrayPointer, Length);
+
+        public static implicit operator ManagedBytesArray<T>(T[] array) => new(array);
+    }
+    
+    public unsafe class ManagedArray<T> : IUnsafeArray<T, T> where T : unmanaged
+    {
+        public int Length
+        {
+            get => length;
+            set
+            {
+                ((IUnsafeArray<T, T>)this).Resize(value, length);
+                length = value;
+            }
+        }
+
+        ushort IUnsafeArray<T, T>.StructSize => structSize;
+
+        T* IUnsafeArray<T, T>.ArrayPointer
+        {
+            get => arrayPointer;
+            set => arrayPointer = value;
+        }
+
+        private readonly ushort structSize;
+
+        private T* arrayPointer;
+        private int length;
+
+        public ManagedArray(int length)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>();
+            this.length = length;
+            arrayPointer = (T*)MemoryUtilities.Allocate(structSize * length);
+        }
+
+        public ManagedArray(T[] array)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>(); 
+            length = array.Length;
+            arrayPointer = (T*)Marshal.UnsafeAddrOfPinnedArrayElement(array, 0).ToPointer();
+        }
+
+        public T* this[int index]
+        {
+            get => &arrayPointer[index];
+            set => arrayPointer[index] = value[0];
+        }
+
+        public T[] ToArray() => MemoryUtilities.FromPointerToArray<T>(arrayPointer, Length);
+
+        public static implicit operator ManagedArray<T>(T[] array) => new(array);
+    }
+}
+#endif
---
+++ Extensions/Memory/Arrays/UnmanagedArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Rietmon.Extensions
+{
+    public unsafe struct UnmanagedBytesArray<T> : IUnsafeArray<T, byte> where T : struct
+    {
+        public int Length
+        {
+            get => length;
+            set
+            {
+                ((IUnsafeArray<T, byte>)this).Resize(value, length);
+                length = value;
+            }
+        }
+
+        byte* IUnsafeArray<T, byte>.ArrayPointer
+        {
+            get => arrayPointer;
+            set => arrayPointer = value;
+        }
+
+        ushort IUnsafeArray<T, byte>.StructSize => structSize;
+
+        private readonly ushort structSize;
+        
+        private int length;
+        private byte* arrayPointer;
+
+        public UnmanagedBytesArray(int length)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>();
+            this.length = length;
+            arrayPointer = (byte*)MemoryUtilities.Allocate(structSize * length);
+        }
+        
+        public UnmanagedBytesArray(T[] array)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>(); 
+            length = array.Length;
+            arrayPointer = (byte*)Marshal.UnsafeAddrOfPinnedArrayElement(array, 0).ToPointer();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= length)
+                    return default;
+            
+                return MemoryUtilities.FromPointer<T>(arrayPointer + index * structSize);
+            }
+            set
+            {
+                if (index >= length)
+                    return;
+            
+                MemoryUtilities.CopyMemory(arrayPointer + index * structSize, value.ToPointer(), structSize);
+            }
+        }
+
+        public T[] ToArray() => MemoryUtilities.FromPointerToStructureArray<T>(arrayPointer, Length);
+
+        public static implicit operator UnmanagedBytesArray<T>(T[] array) => new(array);
+    }
+    
+    public unsafe struct UnmanagedArray<T> : IUnsafeArray<T, T> where T : unmanaged
+    {
+        public int Length
+        {
+            get => length;
+            set
+            {
+                ((IUnsafeArray<T, T>)this).Resize(value, length);
+                length = value;
+            }
+        }
+
+        ushort IUnsafeArray<T, T>.StructSize => structSize;
+
+        T* IUnsafeArray<T, T>.ArrayPointer
+        {
+            get => arrayPointer;
+            set => arrayPointer = value;
+        }
+
+        private readonly ushort structSize;
+        
+        private int length;
+        private T* arrayPointer;
+
+        public UnmanagedArray(int length)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>();
+            this.length = length;
+            arrayPointer = (T*)MemoryUtilities.Allocate(structSize * length);
+        }
+
+        public UnmanagedArray(T[] array)
+        {
+            structSize = (ushort)MemoryUtilities.SizeOf<T>(); 
+            length = array.Length;
+            arrayPointer = (T*)Marshal.UnsafeAddrOfPinnedArrayElement(array, 0).ToPointer();
+        }
+
+        public T this[int index]
+        {
+            get => arrayPointer[index];
+            set => arrayPointer[index] = value;
+        }
+
+        public T[] ToArray() => MemoryUtilities.FromPointerToArray<T>(arrayPointer, Length);
+
+        public static implicit operator UnmanagedArray<T>(T[] array) => new(array);
+    }
+}
