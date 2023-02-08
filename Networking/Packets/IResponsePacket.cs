c29704f1d9adcc110cdb6e3efe1815aae439ffed
Ilya <www.sakhno23@gmail.com>
2023-02-08 20:19:52 +0100

    Update

--- Networking/Packets/IResponsePacket.cs
+++ Networking/Packets/IResponsePacket.cs
@@ -7,15 +7,5 @@ namespace DamnLibrary.Networking.Packets
         public ResponseType Response { get; set; }
 
         public bool IsValid { get; }
-        
-        void ISerializable.Serialize(SerializationStream stream)
-        {
-            stream.Write(Response);
-        }
-
-        void ISerializable.Deserialize(SerializationStream stream)
-        {
-            Response = stream.Read<ResponseType>();
-        }
     }
 }
