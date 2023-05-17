93d5672668345306a6d47a8b95e496839c6ade27
Ilya <www.sakhno23@gmail.com>
2023-05-17 22:45:22 +0200

    Update

--- Management/WindowsManager/Animations/DefaultWindowAnimator.cs
+++ Management/WindowsManager/Animations/DefaultWindowAnimator.cs
@@ -9,7 +9,7 @@ namespace DamnLibrary.Management.Animations
 {
 	public class DefaultWindowAnimator : WindowAnimator
 	{
-		private const float AnimationDuration = 0.2f;
+		private const float AnimationDuration = 0.3f;
 
 		private static readonly Vector3Range horizontalContentScaleValues = new(new Vector3(0.6f, 1), new Vector3(1, 1));
 		private static readonly Vector3Range verticalContentScaleValues = new(new Vector3(1, 0.3f), new Vector3(1, 1));
@@ -37,7 +37,7 @@ namespace DamnLibrary.Management.Animations
 			if (faderImage)
 			{
 				faderImage.color = isAppear ? faderColor.A(0) : faderColor;
-				faderImage.DOColor(faderColor, AnimationDuration);
+				faderImage.DOColor(isAppear ? faderColor : faderColor.A(0), AnimationDuration);
 			}
 
 			var firstIndex = isAppear ? 0 : 1;
