ebd8ae66043f8a4a0fe6fc57b971e7a8d1e0fe27
Ilya <www.sakhno23@gmail.com>
2023-12-23 10:14:19 +0100

    Update

---
+++ Animations/SpriteSequences/SpriteSequenceAnimation.cs
@@ -0,0 +1,16 @@
+using DamnLibrary.Animations.SpriteSequences;
+using UnityEngine;
+
+namespace DamnLibrary.Animations
+{
+    [CreateAssetMenu(fileName = "SpriteSequenceAnimation", menuName = "DamnLibrary/Animations/SpriteSequenceAnimation")]
+    public class SpriteSequenceAnimation : ScriptableObject
+    {
+        [field: SerializeField] public Sprite[] Sprites { get; set; }
+        
+        [field: SerializeField] public SpriteSequenceAnimationType AnimationType { get; set; } = 
+            SpriteSequenceAnimationType.ForwardAndRepeat;
+
+        [field: SerializeField] public float FrameDuration { get; set; } = 0.1f;
+    }
+}
