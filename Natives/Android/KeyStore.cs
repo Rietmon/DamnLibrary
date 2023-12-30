12be6d54b6a46d6a2da0bec2189f7096d221a9dd
Ilya <www.sakhno23@gmail.com>
2023-12-30 15:23:04 +0100

    Fixes

--- Natives/Android/KeyStore.cs
+++ Natives/Android/KeyStore.cs
@@ -1,34 +1,34 @@
-using UnityEngine;
-
-namespace DamnLibrary.Natives.Android
-{
-    public static class KeyStore
-    {
-#if ENABLE_ANDROID && !UNITY_EDITOR
-        private static readonly AndroidJavaClass keyStoreClass = new("android.security.KeyChain");
-#endif
-        
-        public static void WriteToKeystore(string alias, string value)
-        {
-#if ENABLE_ANDROID && !UNITY_EDITOR
-            using var keyStore = keyStoreClass.CallStatic<AndroidJavaObject>("createInstallIntent");
-            using var editor = keyStoreClass.CallStatic<AndroidJavaObject>("getEditor", keyStore);
-            editor.Call("putString", alias, value);
-            editor.Call("apply");
-#else
-            PlayerPrefs.SetString(alias, value);
-#endif
-        }
-        
-        public static string ReadFromKeystore(string alias)
-        {
-#if ENABLE_ANDROID && !UNITY_EDITOR
-            using var keyStore = keyStoreClass.CallStatic<AndroidJavaObject>("createInstallIntent");
-            using var sharedPreferences = keyStoreClass.CallStatic<AndroidJavaObject>("getDefaultSharedPreferences", keyStore);
-            return sharedPreferences.Call<string>("getString", alias, null);
-#else
-            return PlayerPrefs.GetString(alias);
-#endif
-        }
-    }
-}
+// using UnityEngine;
+//
+// namespace DamnLibrary.Natives.Android
+// {
+//     public static class KeyStore
+//     {
+// #if ENABLE_ANDROID && !UNITY_EDITOR
+//         private static readonly AndroidJavaClass keyStoreClass = new("android.security.KeyChain");
+// #endif
+//         
+//         public static void WriteToKeystore(string alias, string value)
+//         {
+// #if ENABLE_ANDROID && !UNITY_EDITOR
+//             using var keyStore = keyStoreClass.CallStatic<AndroidJavaObject>("createInstallIntent");
+//             using var editor = keyStoreClass.CallStatic<AndroidJavaObject>("getEditor", keyStore);
+//             editor.Call("putString", alias, value);
+//             editor.Call("apply");
+// #else
+//             PlayerPrefs.SetString(alias, value);
+// #endif
+//         }
+//         
+//         public static string ReadFromKeystore(string alias)
+//         {
+// #if ENABLE_ANDROID && !UNITY_EDITOR
+//             using var keyStore = keyStoreClass.CallStatic<AndroidJavaObject>("createInstallIntent");
+//             using var sharedPreferences = keyStoreClass.CallStatic<AndroidJavaObject>("getDefaultSharedPreferences", keyStore);
+//             return sharedPreferences.Call<string>("getString", alias, null);
+// #else
+//             return PlayerPrefs.GetString(alias);
+// #endif
+//         }
+//     }
+// }
